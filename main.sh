14217616afd14adf6c0c4bdc85af7157e8a37a54
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2023-12-12 13:22:10 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -101,6 +101,21 @@ if [ $d1 -eq 1 ]
 then
 	echo "ça arrive bientot tkt"
 fi
+
+if [ $l -eq 1 ]
+then
+	sort -t';' -n -k1 -k2 data.csv | head -n5000 > datatest2.csv
+	cat datatest2.csv
+	#sort -t';' -r  -n -k5 data.csv | head -n10 > 10long.csv
+	#cat 10long.csv
+fi
+
+for i in `seq 3 $#`
+do
+
+
+done
+
 fin=$(date +%s)
 duree=$(( $fin -$debut ))
 
