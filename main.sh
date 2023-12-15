6ff4146e6e385b83e00fb39a0a2b078d7ffb352e
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2023-12-15 22:15:25 +0100

    Finish the -l argument sort

--- main.sh
+++ main.sh
@@ -145,7 +145,7 @@ if [ $l -eq 1 ]
 then
 	#sort -t';' -n -k1 -k2 $1 | head -n10 | LC_NUMERIC="C" awk -F';' -W sprintf=num '{sum += $5}END{print sum}'> temp1.csv
 	
-	#En une ligne : sort -t';' -n -k1 -k2 $1 | LC_NUMERIC="C" awk -F';' -W sprintf=num 'BEGIN {a=0; sum=0} {if(a!=$1){print sum ";" $6; a++; sum=0} else {sum+=$5}}' | sort -t';' -n -r -k1 | head -10
+	#En une ligne : sort -t';' -n -k1 -k2 $1 | LC_NUMERIC="C" awk -F';' -W sprintf=num 'BEGIN {a=0; sum=0} {if(a!=$1){print sum ";" a; a++; sum=0} else {sum+=$5}}' | sort -t';' -n -r -k1 | head -10
 	
 	sort -t';' -n -k1 -k2 $1 > temp1.csv
 	LC_NUMERIC="C" awk -F';' -W sprintf=num 'BEGIN {a=0; sum=0} {if(a!=$1){print sum ";" a; a++; sum=0} else {sum+=$5}}' temp1.csv | sort -t';' -n -r -k1 > temp2.csv
