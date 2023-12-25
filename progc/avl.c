3749943a92b99e2270974f46005d90e9beecc66b
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2023-12-25 14:10:11 +0100

    Add the avl with struct Camion

--- progc/avl.c
+++ progc/avl.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define min(i, j) (((i) < (j)) ? (i) : (j))
 #define max(i, j) (((i) > (j)) ? (i) : (j))
 
+typedef struct Camion{
+	int v;
+	char * v2;
+} camion;
 
 typedef struct Avl{
-    int val;
+    camion val;
     int equilibre;
     struct Avl* fg;
     struct Avl* fd;
 }Avl,*pAvl;
 
 
-pAvl creerArbre(int num){
+pAvl creerArbre(camion num){
     pAvl monAvl=(pAvl)malloc(sizeof(Avl));
 
     if(monAvl==NULL){
@@ -106,16 +111,16 @@ pAvl equilibrerAVL(pAvl myAvl){
 }
 
 
-pAvl insertionAVL(pAvl myAvl, int e, int* h){
+pAvl insertionAVL(pAvl myAvl, camion e, int* h){
 	if(myAvl == NULL){
 		*h=1;
 		return creerArbre(e);
 	}
-	else if(e < myAvl->val){
+	else if(e.v < (myAvl->val).v){
 		myAvl->fg = insertionAVL(myAvl->fg, e, h);
 		*h = -*h;
 	}
-	else if(e > myAvl->val){
+	else if(e.v > (myAvl->val).v){
 		myAvl->fd = insertionAVL(myAvl->fd, e, h);
 	}
 	else{
@@ -136,7 +141,7 @@ pAvl insertionAVL(pAvl myAvl, int e, int* h){
 }
 
 
-pAvl suppMinAVL(pAvl myAvl, int* h, int* pe){
+pAvl suppMinAVL(pAvl myAvl, int* h, camion* pe){
 	if(myAvl->fg == NULL){
 		*pe = myAvl->val;
 		*h = -1;
@@ -163,15 +168,15 @@ pAvl suppMinAVL(pAvl myAvl, int* h, int* pe){
 }
 
 
-pAvl suppressionAVL(pAvl myAvl, int e, int* h){
+pAvl suppressionAVL(pAvl myAvl, camion e, int* h){
 	if(myAvl == NULL){
 		*h=1;
 		return myAvl;
 	}
-	else if(e > myAvl->val){
+	else if(e.v > (myAvl->val).v){
 		myAvl->fd = suppressionAVL(myAvl->fd, e, h);
 	}
-	else if(e < myAvl->val){
+	else if(e.v < (myAvl->val).v){
 		myAvl->fg = suppressionAVL(myAvl->fg, e, h);
 		*h = -*h;
 	}
@@ -200,7 +205,65 @@ pAvl suppressionAVL(pAvl myAvl, int e, int* h){
 	return myAvl;
 }
 
-int main(){
+
+void parcoursInfixeInverse(pAvl a){
+	if(a!=NULL){
+		voila(a->fd);
+		//La ça va print nom_de_la_ville;distance
+		printf("%s;%d\n", (a->val).v2, (a->val).v);
+		voila(a->fg);
+	}
+}
+
+
+int main(int arg, char** argv){
+	//Va prendre les fichiers temp/temps2.data et temp/temps2.data en mode lecture et les stocker dans fichier et fichier2
+	FILE * fichier = fopen("temp/temps2.data", "r");
+	FILE * fichier2 = fopen("temp/temps3.data", "r");
+	
+	//Creation d'une chaine de caractere pour la ville et d'un int pour la distance
+	char c[50];
+	int d;
+	
+	//On recupere le nom de la ville et on l'a met dans c, de la meme façon pour la distance et d, le "%[^\n]\n" signifie qu'on prend tant qu'on tombe pas sur un \n
+	fscanf(fichier, "%[^\n]\n", c);
+	fscanf(fichier2, "%d", &d);
+	
+	//On cree une varible de type camion et on met les valeurs qu'on vient de récup dans cam.v et cam.v2, le strdup(c) signifie que c'est une copie de c, sinon ça va bug et prendre toujours la meme chaine de caractere (a cause des pointeurs tout ça)
+	camion cam;
+	cam.v = d;
+	cam.v2 = strdup(c);
+	
+	//Creation avl avec cam en valeur
+	pAvl a=creerArbre(cam);
+	
+	//Prend le nombre de ligne avec l'argument $a qu'on a donne dans main.sh, et on fait -1 car on deja mis le premier element
+	int nbligne = atoi(argv[1])-1;
+	
+	//boucle for pour traverser toutes les lignes des fichiers
+	for(int i=0; i<nbligne; i++){
+		int h=0;
+		
+		//Meme chose que tout a l'heure
+		fscanf(fichier, "%[^\n]\n", c);
+		fscanf(fichier2, "%d", &d);
+		
+		//printf("%s\n", c);
+		
+		//Pareil
+		camion cam;
+		cam.v = d;
+		cam.v2 = strdup(c);
+		
+		//printf("%d\n", i);
+		
+		//La on insert dans l'avl
+		insertionAVL(a, cam, &h);
+	}
+	//On fait le parcours infixe mais inverse
+	parcoursInfixeInverse(a);
+	
+	//Y'a 2 problemes, c'est que ça n'affiche pas toutes les villes/distances et que ça ne prend pas en compte quand 2 distances ont les memes valeurs. Bonne chance pour regler ces bugs
 	
 	return 0;
 }
