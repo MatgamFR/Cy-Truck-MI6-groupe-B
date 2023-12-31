bc3cf5d6793ad5ba6c09b977db8ea8ff2cc1d53b
MatgamFR <80062640+MatgamFR@users.noreply.github.com>
2023-12-31 22:39:04 +0100

    Change to add -s argument

--- progc/avl.c
+++ progc/avl.c
@@ -132,3 +132,135 @@ void parcoursInfixeInverse(pAvl a){
 		parcoursInfixeInverse(a->fg);
 	}
 }
+
+
+pAvl2 creerArbre2(camion2 num){
+    pAvl2 monAvl=(pAvl2)malloc(sizeof(Avl2));
+
+    if(monAvl==NULL){
+        printf("Error of memory Allocation\n");
+        exit(1);
+    }
+
+    monAvl->val=num;
+    monAvl->equilibre=0;
+    monAvl->fg=NULL;
+    monAvl->fd=NULL;
+
+    return monAvl;
+}
+
+
+pAvl2 rotationGauche2(pAvl2 myAvl){
+
+    pAvl2 bivot;
+    int eqA,eqB;
+
+    bivot=myAvl->fd;
+    myAvl->fd=bivot->fg;
+    bivot->fg=myAvl;
+
+    eqA=myAvl->equilibre;
+    eqB=bivot->equilibre;
+
+    myAvl->equilibre=eqA-max(eqB,0)-1;
+    bivot->equilibre= min(eqA-2,min(eqA+eqB-2,eqB-1));
+
+    myAvl=bivot;
+
+    return myAvl;
+}
+
+pAvl2 rotationDroite2(pAvl2 myAvl){
+
+    pAvl2 bivot;
+    int eqA,eqB;
+
+    bivot=myAvl->fg;
+    myAvl->fg=bivot->fd;
+    bivot->fd=myAvl;
+
+    eqA=myAvl->equilibre;
+    eqB=bivot->equilibre;
+
+    myAvl->equilibre=eqA-min(eqB,0)+1;
+    bivot->equilibre= max(eqA+2,max(eqA+eqB+2,eqB+1));
+
+    myAvl=bivot;
+
+    return myAvl;
+}
+
+
+pAvl2 doubleRotationGauche2(pAvl2 myAvl){
+    pAvl2 bivot;
+    myAvl->fd=rotationDroite2(myAvl->fd);
+    
+    return rotationGauche2(myAvl);
+}
+
+pAvl2 doubleRotationDroite2(pAvl2 myAvl){
+    pAvl2 bivot;
+    myAvl->fg=rotationGauche2(myAvl->fg);
+    
+    return rotationDroite2(myAvl);
+}
+
+
+pAvl2 equilibrerAVL2(pAvl2 myAvl){
+    if(myAvl->equilibre >=2 ){
+        if(myAvl->fd->equilibre >= 0){
+            return rotationGauche2(myAvl);
+        }
+        else{
+            return doubleRotationGauche2(myAvl);
+        }
+    }
+    else if(myAvl->equilibre <= -2){
+        if(myAvl->fg->equilibre <=0){
+            return rotationDroite2(myAvl);
+        }
+        else{
+            return doubleRotationDroite2(myAvl);
+        }
+    }
+    return myAvl;
+}
+
+
+pAvl2 insertionAVL2(pAvl2 myAvl, camion2 e, int* h){
+	if(myAvl == NULL){
+		*h=1;
+		return creerArbre2(e);
+	}
+	else if(e.v < (myAvl->val).v){
+		myAvl->fg = insertionAVL2(myAvl->fg, e, h);
+		*h = -*h;
+	}
+	else if(e.v >= (myAvl->val).v){
+		myAvl->fd = insertionAVL2(myAvl->fd, e, h);
+	}
+	else{
+		*h = 0;
+		return myAvl;
+	}
+	if(*h != 0){
+		myAvl->equilibre += *h;
+		myAvl = equilibrerAVL2(myAvl);
+		if(myAvl->equilibre == 0){
+			*h = 0;
+		}
+		else{
+			*h = 1;
+		}
+	}
+	return myAvl;
+}
+
+void parcoursInfixeInverse2(pAvl2 a){
+	if(a!=NULL){
+		parcoursInfixeInverse2(a->fd);
+		printf("%d;%.3f;%.3f;%.3f,%.3f\n", (a->val).v2, (a->val).v3, (a->val).v4, (a->val).v5, (a->val).v);
+		parcoursInfixeInverse2(a->fg);
+	}
+}
--- progc/truck.h
+++ progc/truck.h
@@ -10,12 +10,27 @@ typedef struct Camion{
 	char * v3;
 } camion;
 
+typedef struct Camion2{
+	float v;
+	float v3;
+	float v4;
+	float v5;
+	int v2;
+} camion2;
+
 typedef struct Avl{
     camion val;
     int equilibre;
     struct Avl* fg;
     struct Avl* fd;
 }Avl,*pAvl;
+
+typedef struct Avl2{
+    camion2 val;
+    int equilibre;
+    struct Avl2* fg;
+    struct Avl2* fd;
+}Avl2,*pAvl2;
 
 pAvl creerArbre(camion num);
 pAvl rotationGauche(pAvl myAvl);
@@ -25,3 +40,12 @@ pAvl doubleRotationDroite(pAvl myAvl);
 pAvl equilibrerAVL(pAvl myAvl);
 pAvl insertionAVL(pAvl myAvl, camion e, int* h);
 void parcoursInfixeInverse(pAvl a);
+
+pAvl2 creerArbre2(camion2 num);
+pAvl2 rotationGauche2(pAvl2 myAvl);
+pAvl2 rotationDroite2(pAvl2 myAvl);
+pAvl2 doubleRotationGauche2(pAvl2 myAvl);
+pAvl2 doubleRotationDroite2(pAvl2 myAvl);
+pAvl2 equilibrerAVL2(pAvl2 myAvl);
+pAvl2 insertionAVL2(pAvl2 myAvl, camion2 e, int* h);
+void parcoursInfixeInverse2(pAvl2 a);
